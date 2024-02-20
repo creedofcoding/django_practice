dd918c2272396140053300bbe1d723e3d126cf0c
jus_iljas <khairullin.iljas@gmail.com>
2024-02-20 15:31:36 +0300

    made pagination option

--- goods/views.py
+++ goods/views.py
@@ -1,19 +1,23 @@
-from unicodedata import category
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Product
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == 'all':
         goods = Product.objects.all()
     else:
         goods = get_list_or_404(Product.objects.filter(category__slug=category_slug))
 
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
+
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
 
     return render(request, "goods/catalog.html", context)
