27f7ecfc7fccd48eaa68e161232c942a32a69da3
jus_iljas <khairullin.iljas@gmail.com>
2024-03-08 21:21:00 +0300

    added to cart view, refactored included cart templates

--- users/views.py
+++ users/views.py
@@ -21,7 +21,8 @@ def login(request):
                     request, f"{username}, вы успешно вошли в свой аккаунт!"
                 )
 
-                if request.POST.get("next", None):
+                redirect_page = request.POST.get("next", None)
+                if redirect_page and redirect_page != reverse("user:logout"):
                     return HttpResponseRedirect(request.POST.get("next"))
 
                 return HttpResponseRedirect(reverse("main:index"))
