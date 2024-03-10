dbf6c818ae6756170a69f9f0a95feb891f454fda
jus_iljas <khairullin.iljas@gmail.com>
2024-03-10 22:17:48 +0300

    admin panel setup + minor fixes

--- goods/admin.py
+++ goods/admin.py
@@ -8,8 +8,28 @@ from goods.models import Category, Product
 @admin.register(Category)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = [
+        "name",
+    ]
 
 
 @admin.register(Product)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "quantity", "price", "discount"]
+    list_editable = [
+        "quantity", 
+        "price", 
+        "discount"
+    ]
+    search_fields = ["name", "description"]
+    list_filter = ["discount", "quantity", "category"]
+    fields = [
+        "category",
+        "name",
+        "slug",
+        "description",
+        "image",
+        ("price", "discount"),
+        "quantity",
+    ]
--- goods/models.py
+++ goods/models.py
@@ -2,10 +2,14 @@ from email.policy import default
 from tabnanny import verbose
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
 
     class Meta:
         db_table = "category"
@@ -17,14 +21,24 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
     description = models.TextField(blank=True, null=True, verbose_name="Описание")
-    image = models.ImageField(upload_to="goods_images", blank=True, null=True, verbose_name="Изображение")
-    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена")
-    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка в %')
-    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, verbose_name="Категория")
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
+    )
+    discount = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category = models.ForeignKey(
+        to=Category, on_delete=models.CASCADE, verbose_name="Категория"
+    )
 
     class Meta:
         db_table = "product"
@@ -33,13 +47,16 @@ class Product(models.Model):
         ordering = ("id",)
 
     def __str__(self):
-        return self.name
-    
+        return f"{self.name} Количество - {self.quantity}"
+
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+
     def display_id(self):
         return f"{self.id:05}"
-    
+
     def sell_price(self):
         if self.discount:
             return round(self.price - (self.price * self.discount / 100), 2)
-        
+
         return self.price
